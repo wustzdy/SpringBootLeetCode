bf61ff610760e53286b8683f29523ee165d75af6
wustzdy <714403855@qq.com>
2020-08-26 23:00:10 +0800

    optimize

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/RemoveDuplicateTest1.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/RemoveDuplicateTest1.java
@@ -2,6 +2,13 @@ package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm;
 
 //26. 删除排序数组中的重复项
 //https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shuang-zhi-zhen-shan-chu-zhong-fu-xiang-dai-you-hu/
+
+/**
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ **/
 /*解题思路：
         解法： 双指针
         首先注意数组是有序的，那么重复的元素一定会相邻。

84c0d8811cb8ffeca358fda82d46e22eff298f3b
zhudayang <zhudayang@sensetime.com>
2021-01-28 14:14:49 +0800

    【数据结构】-线性表-【2018真题】找出未出现的最小正整数

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/MinNumberTest.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/MinNumberTest.java
@@ -42,22 +42,21 @@ public class MinNumberTest {
     }
 
     public static int missNum(int[] arr) {
-        {
-            int l = 0; //l表示已经从1到L已经出现（左边界），l的初值为0。
-            int r = arr.length; //如果一个数字过大（不合法），就会被扔掉，用r表示这个右边界，r初始值为arr长度。
-            int temp;
-            while (l < r) {
-                if (arr[l] == l + 1) {
-                    l++;
-                } else if (arr[l] > r || arr[l] <= l || arr[arr[l] - 1] == arr[l]) {//不合法
-                    arr[l] = arr[--r];
-                } else {//合法但是没有在理想的位置上
-                    temp = arr[l];
-                    arr[l] = arr[arr[l] - 1];
-                    arr[temp - 1] = temp;
-                }
+        int l = 0; //l表示已经从1到L已经出现（左边界），l的初值为0。
+        int r = arr.length; //如果一个数字过大（不合法），就会被扔掉，用r表示这个右边界，r初始值为arr长度。
+        int temp;
+        while (l < r) {
+            if (arr[l] == l + 1) {
+                l++;
+            } else if (arr[l] > r || arr[l] <= l || arr[arr[l] - 1] == arr[l]) {//不合法
+                arr[l] = arr[--r];
+            } else {//合法但是没有在理想的位置上
+                temp = arr[l];
+                arr[l] = arr[arr[l] - 1];
+                arr[temp - 1] = temp;
             }
-            return l + 1;
         }
+        return l + 1;
     }
+
 }

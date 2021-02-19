139877ae7ed2a8e3b30668aae910113083cea3c5
zhudayang <zhudayang@sensetime.com>
2021-02-19 16:16:13 +0800

    爬楼梯优化

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/climbStairs.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/climbStairs.java
@@ -2,6 +2,7 @@ package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm;
 
 import java.io.InputStream;
 import java.util.Scanner;
+
 /*题目描述
 
         一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
@@ -24,6 +25,7 @@ public class climbStairs {
         System.out.println("跳法总数：" + "\n" + getClimbStairsLayer(n));
     }
 
+    //递归
     private static int getClimbStairsLayer(int n) {
         if (n == 1) {
             return 1;
@@ -32,6 +34,34 @@ public class climbStairs {
         } else {
             return getClimbStairsLayer(n - 1) + getClimbStairsLayer(n - 2);
         }
+    }
+
+    /**
+     * 非递归实现
+     **/
+    public int JumpFloor(int target) {
+        //青蛙跳台阶，一次可以跳上1级台阶，也可以跳上2级台阶
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 2;
+        }
+        //一个n级台阶，第一次跳有两种选择，
+        //一是第一次跳1级，那么跳法数目等于后面剩下的n-1级台阶的跳法数目
+        //二是第一次跳2级，那么跳法数目等于后面剩下的n-2级台阶的跳法数目
+        //一个n级台阶总的跳法数目为f(n-1) + f(n-2),相当于斐波那契数列
+        int first = 1;
+        int last = 2;
+        int sum = 0;
+        for (int i = 3; i <= target; i++) {
+            sum = first + last;
+            //将上次的last作为下一次的first
+            first = last;
+            //将上次的结果作为last
+            last = sum;
+        }
+        return sum;
     }
 }
 

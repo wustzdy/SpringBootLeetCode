051a101d327615b25f8df5a7100604901a71e1df
wustzdy <714403855@qq.com>
2021-03-08 22:58:59 +0800

    1470. 重新排列数组

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/StringLeftRetote.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/StringLeftRetote.java
@@ -3,6 +3,14 @@ package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm;
 //王道课后习题2.2.10:将R中的序列循环左移P个位置
 //设将n(n>1)个整数存放到一维数组R中。设计一个在时间和空间两方面尽可能高效的算法。
 // 将R中的序列循环左移P（0<P<n）个位置，即将R中的数据由（x0，x1，…，xn-1）变换为（xp，Xp+1，…，Xn-1，x0，x1，…，Xp-1）。要求：
+//输入：0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//输出：5 6 7 8 9 0 1 2 3 4
+//https://blog.csdn.net/weixin_42426690/article/details/94486310?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_title-0&spm=1001.2101.3001.4242
+/*2020王道P18，综合10：将一维数组A中的数列循环左移P个位置。个人感觉这一题与第八题很相似，先说说思路：
+        例如数组：{1,2,3,4,5,6,7,8,9}
+        1.将前P个数逆置。----->{4,3,2,1,5,6,7,8,9}
+        2.将后n-p个数逆置。----->{4,3,2,1,9,8,7,6,5}
+        3.将整个数组逆置。----->{5,6,7,8,9,1,2,3,4}*/
 public class StringLeftRetote {
     public static void main(String[] args) {
         int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -13,7 +21,7 @@ public class StringLeftRetote {
         Reverse(0, n - 1, array);//逆置整个数组
 
         for (int i : array) {
-            System.out.print(i + " ");
+            System.out.print(i + ",");
         }
     }
 
---
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/StringLeftRetote1.java
@@ -0,0 +1,48 @@
+package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm;
+
+//顺序表逆置，链表逆置
+//顺序表逆置真题：
+//
+//1.将一长度为n的数组的前端k(k<n)}个元素逆置后移动到数组后端，要求原数组中数据不丢失;
+//
+//2.将一长度为n的数组的前端k(k<n)个元素保持原序移动到数组后端，要求原数组中数据不丢失;
+//
+//3.将数组中的元素(Xo, X1, ... Xn-1)，经过移动后变为:(Xp, Xp+1, ...Xn-1, Xo, X1, ..Xp-1) ，即循环左移p(0<p<n)个位置。
+public class StringLeftRetote1 {
+    public static void main(String[] args) {
+        int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int p = 5;
+        reverse(array, 0, array.length-1, p);////逆置前P
+        for (int i : array) {
+            System.out.print(i + ",");
+        }
+    }
+
+    /*参数说明：
+        a[]:要改变的数组
+       left:左边起点
+      right:右边终点
+          k:要移动的元素个数*/
+    static void reverse(int a[], int left, int right, int k) {
+        int temp;
+        for (int i = left, j = right; i < left + k && i < j; ++i, --j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    //第二题
+    void moveToEnd(int a[], int n, int k) {
+        reverse(a, 0, k - 1, k); //reverse函数为第一题中的
+        reverse(a, 0, n - 1, k);
+    }
+
+
+    //第三题
+    static void moveP(int a[], int n, int p) {
+        reverse(a, 0, p - 1, p);
+        reverse(a, p, n - 1, n - p);
+        reverse(a, 0, n - 1, n);
+    }
+}

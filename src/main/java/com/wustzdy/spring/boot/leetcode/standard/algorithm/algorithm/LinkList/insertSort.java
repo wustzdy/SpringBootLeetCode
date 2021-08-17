a487294d8cf1f990507d8a2ef572a23de12c235c
zhudayang <zhudayang@sensetime.com>
2021-08-17 14:17:02 +0800

    删除排序数组中的重复项

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/LinkList/insertSort.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/LinkList/insertSort.java
@@ -1,42 +1,57 @@
 package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.LinkList;
 
-/*
- * 11.在数组{1,2,3,4,6,7,9,8,10}中插入一个数5，
- * 使其插入完成后仍然有序，运行结果如下：
- */
-public class insertSort {
+public class InsertSort {
     public static void main(String[] args) {
-        int a = 4;
-        int b = 0;//用于纪录需要插入数字的下标
-        int[] array = {1, 2, 3, 4, 6, 7, 8, 9, 10};
-        System.out.println("原来的数组为：");
-        for (int i : array) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-        //找下标，并用b纪录
-        for (int i = 0; i < array.length - 1; i++) {
-            if (a < array[i]) {
-                b = i;
+        int[] arr = {1, 4, 7, 8, 10};
+        int x = 5;
+        int length = arr.length;
+        sort(arr, x, length);
+    }
+
+    static void sort(int arr[], int x, int length) {
+        int low = 0, high = length - 1, mid = 0;                  //low和high分别指向线性表的上界和下界
+        while (low <= high) {
+            mid = (low + high) / 2;                       //中间位置
+            if (arr[mid] == x)                               //找到退出循环
                 break;
-            }
-
+            else if (arr[mid] < x)                        //在右边查找
+                low = mid + 1;
+            else                                         //在左边查找
+                high = mid - 1;
         }
-        //定义一个新数组，大于下标的数后移一位
-        int[] newarr = new int[array.length + 1];
-        for (int i = newarr.length - 1; i >= 0; i--) {
-            if (i > b) {        //当数组下标比插入数的下标大时，后移一位
-                newarr[i] = array[i - 1];
-            } else if (i == b) {    //下标相等时赋值
-                newarr[i] = a;
-
-            } else if (i < b) {    //数组下标比插入数下标小，相等赋值
-                newarr[i] = array[i];
-            }
+        System.out.println("---mid:" + mid);
+        if (arr[mid] == x && mid != length - 1)                  //找到且不是最后一个元素与其后面元素交换位置
+        {
+            int t;
+            t = arr[mid];
+            arr[mid] = arr[mid + 1];
+            arr[mid + 1] = t;
         }
-        System.out.println("插入后的数组：");
-        for (int i : newarr) {
-            System.out.print(i + ", ");
+        if (low > high)                                    //没有找到，插入元素
+        {
+            int i;
+            for (i = length - 2; i > high; i--)                   //元素后移
+            {
+                arr[i + 1] = arr[i];
+            }
+            //表长增加1
+            arr[i + 1] = x;                             //插入元素
+        }
+
+//        arr.[length - 1]=10;
+        for (int element : arr) {
+            System.out.print(element + ",");
+        }
+    }
+
+    public void insert(int[] a, int lower, int upper, int x) {
+        for (int i = upper; i >= lower; i--) {
+            if (a[i] > x) {
+                a[i + 1] = a[i];
+            } else {
+                a[i + 1] = x;
+                return;
+            }
         }
     }
 }

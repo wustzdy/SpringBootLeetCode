efe0809e817b0f8ac4e164d2f812110a017d181a
wustzdy <714403855@qq.com>
2021-09-20 20:54:51 +0800

    //冒泡排序

---
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/sort/BubbleSort1.java
@@ -0,0 +1,26 @@
+package com.wustzdy.spring.boot.leetcode.standard.algorithm.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort1 {
+    public static void main(String[] args) {
+        int[] array = {49, 38, 65, 97, 76, 13, 27, 49};
+        bubbleSort(array);
+        System.out.println("After: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSort(int[] array) {
+        //外层循环控制比较次数
+        for (int i = 0; i < array.length - 1; i++) {
+            //内层循环控制到达位置
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                //比较前面的元素比后面元素大交换，同理也可以比较小
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/sort/BubbleSort2.java
@@ -0,0 +1,34 @@
+package com.wustzdy.spring.boot.leetcode.standard.algorithm.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort2 {
+    public static void main(String[] args) {
+        int[] array = {49, 38, 65, 97, 76, 13, 27, 49};
+        bubbleSort2(array);
+        System.out.println("After: " + Arrays.toString(array));
+    }
+
+    //减少一些可能循环的次数
+    public static void bubbleSort2(int[] numbers) {
+        //外层循环控制比较次数
+        for (int i = 0; i < numbers.length - 1; i++) {
+
+            int flag = 0;//默认标记为0
+            //内层循环控制到达位置
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                //比较前面的元素比后面元素大交换，同理也可以比较小
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                    flag = 1;//如果还有交换，标记为1
+                }
+            }
+
+            if (flag == 0) {//如果没有交换过的元素，则已经有序了
+                return;
+            }
+        }
+    }
+}

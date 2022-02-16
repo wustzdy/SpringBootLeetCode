90755ce1f926a049f177d4196d24f2513fc9747e
wustzdy <714403855@qq.com>
2022-02-16 21:53:59 +0800

    最大子序和

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/leetCode20220213/Test_06_MaxSubArray.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/leetCode20220213/Test_06_MaxSubArray.java
@@ -21,7 +21,12 @@ public class Test_06_MaxSubArray {
         int max = maxSubArray(nums);
         System.out.println("max: " + max);
     }
-
+   /* 思路
+    这道题用动态规划的思路并不难解决，比较难的是后文提出的用分治法求解，但由于其不是最优解法，所以先不列出来
+    动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+    如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
+    如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
+    每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果*/
     //动态规划
     //若前一个元素大于0，则将其加入到当前元素上
     public static int maxSubArray(int[] arr) {

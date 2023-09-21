f15ba74cc5bf8f4516dd843debf546527f9a7f0d
zhudayang <zhudayang@sensetime.com>
2023-09-21 10:14:46 +0800

    19.删除链表的倒数第N个节点

---
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/leetCode20220911/deleteNode.java
@@ -0,0 +1,80 @@
+package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.leetCode20220911;
+//5, 19.删除链表的倒数第N个节点
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//输入：head = [1,2,3,4,5], n = 2 输出：[1,2,3,5] 示例 2：
+//https://programmercarl.com/0019.%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E7%9A%84%E5%80%92%E6%95%B0%E7%AC%ACN%E4%B8%AA%E8%8A%82%E7%82%B9.html#%E6%80%9D%E8%B7%AF
+
+/**
+ * 使用虚拟头结点，这样方便处理删除实际头结点的逻辑
+ * 1,定义fast指针和slow指针，初始值为虚拟头结点
+ * 2,ast首先走n + 1步 ，为什么是n+1呢，因为只有这样同时移动的时候slow才能指向删除节点的上一个节点（方便做删除操作），
+ */
+public class deleteNode {
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode oldLode = new ListNode(1, node2);
+        System.out.println(oldLode);
+
+        System.out.println("删除链表的倒数第2个节点为：");
+        ListNode newNode = removeNthFromEnd(oldLode, 2);
+
+        System.out.println(newNode);
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+
+        ListNode fastIndex = dummyNode;
+        ListNode slowIndex = dummyNode;
+
+        //只要快慢指针相差 n 个结点即可
+        for (int i = 0; i < n; i++) {
+            fastIndex = fastIndex.next;
+        }
+
+        while (fastIndex.next != null) {
+            fastIndex = fastIndex.next;
+            slowIndex = slowIndex.next;
+        }
+
+        //此时 slowIndex 的位置就是待删除元素的前一个位置。
+        //具体情况可自己画一个链表长度为 3 的图来模拟代码来理解
+        slowIndex.next = slowIndex.next.next;
+        return dummyNode.next;
+    }
+
+    private static class ListNode {
+        private int val;
+        private ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder(64);
+            sb.append("[");
+            ListNode p = this;
+            while (p != null) {
+                sb.append(p.val);
+                if (p.next != null) {
+                    sb.append(",");
+                }
+                p = p.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+}

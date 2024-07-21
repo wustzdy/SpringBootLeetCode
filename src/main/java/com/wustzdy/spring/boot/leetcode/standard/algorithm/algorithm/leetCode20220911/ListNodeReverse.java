7ebe60f6685561e1fedcda65adc9c5350a8929e8
wustzdy <714403855@qq.com>
2024-07-21 22:41:26 +0800

    fix

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/leetCode20220911/ListNodeReverse.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/leetCode20220911/ListNodeReverse.java
@@ -1,38 +1,32 @@
 package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.leetCode20220911;
 
+import com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.hoot100.linkNode.reverseList;
+
 //单链表反转
-<<<<<<< HEAD
-=======
 /*[1,2,3,4,5]
   链表反转后：
   [5,4,3,2,1]
 */
->>>>>>> e1da162ab48617a44964aebbe3268ed7ba179a0e
 public class ListNodeReverse {
     public static void main(String[] args) {
         ListNode node5 = new ListNode(5, null);
         ListNode node4 = new ListNode(4, node5);
         ListNode node3 = new ListNode(3, node4);
         ListNode node2 = new ListNode(2, node3);
-<<<<<<< HEAD
         ListNode node1 = new ListNode(1, node2);
         System.out.println("oldNode:" + node1);
-        ListNode result = new ListNodeReverse().revereListNode(node1);
+        ListNode result = reverseList(node1);
         System.out.println("newNode:" + result);
     }
 
-    private ListNode revereListNode(ListNode oldNode) {
+    private static ListNode reverseList(ListNode oldNode) {
         ListNode newNode = null;
         ListNode p = oldNode;
         while (p != null) {
-            newNode = new ListNode(p.val, newNode);//头插法
-=======
-        ListNode oldLode = new ListNode(1, node2);
-        System.out.println(oldLode);
-
-        System.out.println("链表反转后：");
-        ListNode newNode = ReverseList(oldLode);
-        System.out.println(newNode);
+            newNode = new ListNode(p.val, newNode);
+            p = p.next;
+        }
+        return newNode;
     }
 
     private static ListNode ListNodeReverseFunction(ListNode oldNode) {
@@ -40,15 +34,11 @@ public class ListNodeReverse {
         ListNode p = oldNode;
         while (p != null) {
             newNode = new ListNode(p.val, newNode);
->>>>>>> e1da162ab48617a44964aebbe3268ed7ba179a0e
             p = p.next;
         }
         return newNode;
     }
 
-<<<<<<< HEAD
-    static class ListNode {
-=======
     public ListNode ReverseList1(ListNode head) {
         //        //  思路：用新头结点替换原头节点，并将原头节点后移一位，然后再次循环替换、移位，直至原头结点为空，最后返回新的头节点
         // write code here
@@ -69,7 +59,7 @@ public class ListNodeReverse {
         return newHead;
     }
 
-//    https://leetcode.cn/problems/reverse-linked-list/solutions/2361282/206-fan-zhuan-lian-biao-shuang-zhi-zhen-r1jel/
+    //    https://leetcode.cn/problems/reverse-linked-list/solutions/2361282/206-fan-zhuan-lian-biao-shuang-zhi-zhen-r1jel/
     public static ListNode ReverseList(ListNode head) {
         if (head == null) {
             return null;
@@ -85,13 +75,16 @@ public class ListNodeReverse {
     }
 
     private static class ListNode {
->>>>>>> e1da162ab48617a44964aebbe3268ed7ba179a0e
         private int val;
         private ListNode next;
 
         public ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+
+        public ListNode(int val) {
+            this.val = val;
         }
 
         @Override

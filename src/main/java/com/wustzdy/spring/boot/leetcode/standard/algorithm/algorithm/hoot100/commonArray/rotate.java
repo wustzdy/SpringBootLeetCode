4ac7a9ee4741c2d35c273496175632b23eef5891
wustzdy <714403855@qq.com>
2024-07-21 22:26:24 +0800

    数组翻转

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/commonArray/rotate.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/commonArray/rotate.java
@@ -1,4 +1,5 @@
 package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.hoot100.commonArray;
+
 //轮转数组：给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
 /*
 输入: nums = [1,2,3,4,5,6,7], k = 3
@@ -37,9 +38,9 @@ public class rotate {
 
     public static int[] rotate(int[] nums, int k) {
         k %= nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1); //翻转所有元素	7 6 5 4 3 2 1
+        reverse(nums, 0, k - 1); // 翻转 0 到 k-1元素 5 6 7 4 3 2 1
+        reverse(nums, k, nums.length - 1);//翻转 k到 n-1个元素 5 6 7 1 2 3 4
         return nums;
     }
 
@@ -48,8 +49,8 @@ public class rotate {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
-            start += 1;
-            end -= 1;
+            start++;
+            end--;
         }
     }
 }

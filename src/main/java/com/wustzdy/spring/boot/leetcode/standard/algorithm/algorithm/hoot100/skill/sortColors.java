839d844bd4b2c4b9797c59577c8f41016e9d7f3e
wustzdy <714403855@qq.com>
2024-07-26 22:40:59 +0800

    set color

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/skill/sortColors.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/skill/sortColors.java
@@ -9,8 +9,32 @@ package com.wustzdy.spring.boot.leetcode.standard.algorithm.algorithm.hoot100.sk
  */
 public class sortColors {
     public static void main(String[] args) {
-        int[] array=new int[]{2,0,2,1,1,0};
-        sortColors(array);
+        int[] array = new int[]{2, 0, 2, 1, 1, 0};
+        sortColors1(array);
+    }
+//    方法一：单指针
+//    我们可以考虑对数组进行两次遍历。在第一次遍历中，我们将数组中所有的 0 交换到数组的头部。
+//    在第二次遍历中，我们将数组中所有的 1 交换到头部的 0 之后。此时，所有的 2 都出现在数组的尾部，
+//    这样我们就完成了排序。
+    public static void sortColors1(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
     }
 
     public static void sortColors(int[] nums) {

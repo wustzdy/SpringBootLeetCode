9fe717206f35fb5019ab74dc407623e3e357f4a9
wustzdy <714403855@qq.com>
2024-09-01 22:04:35 +0800

    add annotation

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/dynamic/climbStairs.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/dynamic/climbStairs.java
@@ -21,10 +21,42 @@ public class climbStairs {
         System.out.println("请输入台阶数：");
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        System.out.println("跳法总数：" + "\n" + jumpFloor1(n));
+        System.out.println("跳法总数：" + "\n" + jumpFloor(n));
 
     }
 
+//    在到达第n层的上一步，我们只有两个选择，走一步，或者走两步。
+//    如果是走一步，我们需要先通过 f(n-1)种方式到达 n-1 层
+//    如果是走两步， 我们需要通过 f(n-2)种方式到达第 n - 2 层
+//    所以综上有 f(n) = f(n-2) + f(n-1)
+//https://www.bilibili.com/video/BV16v41177GF/?spm_id_from=333.337.search-card.all.click&vd_source=5363405f0e14a0e8f06bcae41548f41e
+/**
+ * 我们只有两个选择，走一步，或者走两步。
+ * 比如要走3步，那么必须要先到达第1步，或者第二步，所以f(3)=f(2)+f(1)
+ * */
+/**
+ * 爬到第一层楼梯有一种方法，爬到二层楼梯有两种方法。
+ *
+ * 那么第一层楼梯再跨两步就到第三层 ，第二层楼梯再跨一步就到第三层。
+ *
+ * 所以到第三层楼梯的状态可以由第二层楼梯 和 到第一层楼梯状态推导出来，那么就可以想到动态规划了。
+ *
+ * 1，确定dp数组以及下标的含义
+ * dp[i]： 爬到第i层楼梯，有dp[i]种方法
+ *
+ * 2，确定递推公式
+ * 如何可以推出dp[i]呢？
+ *
+ * 从dp[i]的定义可以看出，dp[i] 可以有两个方向推出来。
+ *
+ * 首先是dp[i - 1]，上i-1层楼梯，有dp[i - 1]种方法，那么再一步跳一个台阶不就是dp[i]了么。
+ *
+ * 还有就是dp[i - 2]，上i-2层楼梯，有dp[i - 2]种方法，那么再一步跳两个台阶不就是dp[i]了么。
+ *
+ * 那么dp[i]就是 dp[i - 1]与dp[i - 2]之和！
+ *
+ * 所以dp[i] = dp[i - 1] + dp[i - 2] 。
+ * */
     //递归-递归法
     private static int getClimbStairsLayer(int n) {
         if (n == 1) {
@@ -65,7 +97,7 @@ public class climbStairs {
     }
 
     public static int jumpFloor(int number) {
-        int[] dp = new int[number + 1];//如果不初始化dp[0]，那就存数组的时候从1开始
+        int[] dp = new int[number+1];//如果不初始化dp[0]，那就存数组的时候从1开始
         if (number == 1) {  //如果数组下标从1开始，那么这里1的情况就要单独加进去
             return 1;
         }
@@ -76,17 +108,18 @@ public class climbStairs {
         }
         return dp[number];
     }
-    public static int jumpFloor1(int number) {
-        int[] dp = new int[number];//如果不初始化dp[0]，那就存数组的时候从1开始
-        if (number == 1) {  //如果数组下标从1开始，那么这里1的情况就要单独加进去
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        if (n == 1) {
             return 1;
         }
-        dp[0] = 1;//到第一个台阶
-        dp[1] = 2;//到第二个台阶
-        for (int i = 2; i < number; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<n+1;n++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return dp[number-1];
+        return dp[n];
+
     }
 }
 

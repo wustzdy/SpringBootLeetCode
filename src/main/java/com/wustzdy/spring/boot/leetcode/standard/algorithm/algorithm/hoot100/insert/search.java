de5d003e84ddc0dfb9f738a033a50182302f63e8
zhudayang <zhudayang@sensetime.com>
2024-11-20 00:12:00 +0800

    optimize

--- src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/insert/search.java
+++ src/main/java/com/wustzdy/spring/boot/leetcode/standard/algorithm/algorithm/hoot100/insert/search.java
@@ -59,6 +59,16 @@ public class search {
     }
     //standard
     //https://www.bilibili.com/video/BV1tz421r7xC/?spm_id_from=333.337.search-card.all.click&vd_source=5363405f0e14a0e8f06bcae41548f41e
+    //https://leetcode.cn/problems/search-in-rotated-sorted-array/solutions/22068/ji-bai-liao-9983de-javayong-hu-by-reedfan/?envType=study-plan-v2&envId=top-100-liked
+    /*
+    * 题目要求 O(logN) 的时间复杂度，基本可以断定本题是需要使用二分查找，怎么分是关键。
+    由于题目说数字了无重复，举个例子：
+    1 2 3 4 5 6 7 可以大致分为两类，
+    第一类 2 3 4 5 6 7 1 这种，也就是 nums[start] <= nums[mid]。此例子中就是 2 <= 5。
+    这种情况下，前半部分有序。因此如果 nums[start] <=target<nums[mid]，则在前半部分找，否则去后半部分找。
+    第二类 6 7 1 2 3 4 5 这种，也就是 nums[start] > nums[mid]。此例子中就是 6 > 2。
+    这种情况下，后半部分有序。因此如果 nums[mid] <target<=nums[end]，则在后半部分找，否则去前半部分找。
+    * */
     public static int search1(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
